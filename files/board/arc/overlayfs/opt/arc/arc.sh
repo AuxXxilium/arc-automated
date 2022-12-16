6c79870ef1694c5332232974bb767202425894be
AuxXxilium <info@visionz.media>
2022-12-16 18:29:06 +0100

    arc: fixing logic
    Signed-off-by: AuxXxilium <info@visionz.media>

--- files/board/arc/overlayfs/opt/arc/arc.sh
+++ files/board/arc/overlayfs/opt/arc/arc.sh
@@ -84,7 +84,7 @@ if [ $? -ne 0 -o -z "${TAG}" ]; then
       --msgbox "Error checking new version" 0 0
     continue
 fi
-if [ ${TAG} -gt ${ACTUALVERSION} ]; then
+if [ "${TAG}" -gt "${ACTUALVERSION}" ]; then
 dialog --backtitle "`backtitle`" --title "Update ARC" --aspect 18 \
   --infobox "Downloading last version ${TAG}" 0 0
 # Download update file
